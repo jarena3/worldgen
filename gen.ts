0d63378ae18c8c55516695a59946dbf6c347be52
John Arena <john.arena@qsrsoft.com>
2016-08-12 13:44:34 -0500

    updates

--- gen.ts
+++ gen.ts
@@ -11,4 +11,27 @@ function gen(mapX: number, mapY: number) {
 
 }
 
-gen(500, 300);
+$("#seaLevel").slider();
+$("#seaLevel").on("slide", function(slideEvt) {
+	$("#seaLevelSliderVal").text(slideEvt.value.toString());
+});
+
+$("#continentOctaves").slider();
+$("#continentOctaves").on("slide", function(slideEvt) {
+	$("#continentOctavesSliderVal").text(slideEvt.value.toString());
+});
+$("#continentFrequency").slider();
+$("#continentFrequency").on("slide", function(slideEvt) {
+	$("#continentFrequencySliderVal").text(slideEvt.value.toString());
+});
+
+$("#shelfOctaves").slider();
+$("#shelfOctaves").on("slide", function(slideEvt) {
+	$("#shelfOctavesSliderVal").text(slideEvt.value.toString());
+});
+$("#shelfFrequency").slider();
+$("#shelfFrequency").on("slide", function(slideEvt) {
+	$("#shelfFrequencySliderVal").text(slideEvt.value.toString());
+});
+
+//gen(500, 300);
--- js/gen.js
+++ js/gen.js
@@ -5,4 +5,23 @@ function gen(mapX, mapY) {
     var end = Date.now();
     console.log('Rendered in ' + (end - start) + ' ms');
 }
-gen(500, 300);
+$("#seaLevel").slider();
+$("#seaLevel").on("slide", function (slideEvt) {
+    $("#seaLevelSliderVal").text(slideEvt.value.toString());
+});
+$("#continentOctaves").slider();
+$("#continentOctaves").on("slide", function (slideEvt) {
+    $("#continentOctavesSliderVal").text(slideEvt.value.toString());
+});
+$("#continentFrequency").slider();
+$("#continentFrequency").on("slide", function (slideEvt) {
+    $("#continentFrequencySliderVal").text(slideEvt.value.toString());
+});
+$("#shelfOctaves").slider();
+$("#shelfOctaves").on("slide", function (slideEvt) {
+    $("#shelfOctavesSliderVal").text(slideEvt.value.toString());
+});
+$("#shelfFrequency").slider();
+$("#shelfFrequency").on("slide", function (slideEvt) {
+    $("#shelfFrequencySliderVal").text(slideEvt.value.toString());
+});
